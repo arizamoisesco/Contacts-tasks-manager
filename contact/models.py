eebc38d96d2554bd55a9f727d544724df52c738f
Moises Ariza <arizamoises@gmail.com>
2023-03-15 21:40:35 -0500

    Created search, add styles and functionalies for contact app

--- contact/models.py
+++ contact/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from datetime import date
 
 class Contact(models.Model):
-    names = models.CharField(max_length=50, blank=False, null=False)
+    name = models.CharField(max_length=50, blank=False, null=False)
     last_name = models.CharField(max_length=50, blank=True)
     phone = models.CharField(max_length=12, blank=False, null=True)
     mobile = models.CharField(max_length=12, blank=False, null=True)
